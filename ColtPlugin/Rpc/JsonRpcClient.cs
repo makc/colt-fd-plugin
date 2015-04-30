de64adf6171c0ccc865ffd6cd81457faa57da9a3
SlavaRa <ratdg@ya.ru>
2015-04-30 08:46:50 +0300

    Up for FD5

--- ColtPlugin/Rpc/JsonRpcClient.cs
+++ ColtPlugin/Rpc/JsonRpcClient.cs
@@ -1,20 +1,24 @@
-﻿namespace ColtPlugin.Rpc
-{
-    using System;
-    using System.IO;
-    using System.Net;
-    using System.Reflection;
-    using System.Text;
-    using System.Web.Services.Protocols;
-    using Jayrock.Json;
-    using System.Xml;
-    using System.Timers;
-    using System.Diagnostics;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Services.Protocols;
+using System.Windows.Forms;
+using System.Xml;
+using ColtPlugin.Resources;
+using Jayrock.Json;
+using PluginCore;
+using PluginCore.Managers;
+using Timer = System.Timers.Timer;
 
+namespace ColtPlugin.Rpc
+{
     public class JsonRpcException : Exception
     {
-        public String TypeName;
-        public JsonRpcException(String typeName, String message) : base((typeName == null) ? "" : ("[" + typeName + "] ") + message)
+        public string TypeName;
+        public JsonRpcException(string typeName, string message) : base((typeName == null) ? "" : ("[" + typeName + "] ") + message)
         {
             TypeName = typeName;
         }
@@ -24,31 +28,30 @@
     /// Based on sample client from Jayrock author
     /// http://markmail.org/message/xwlaeb3nfanv2kgm
     /// </summary>
-    [System.ComponentModel.DesignerCategory("")]
+    [DesignerCategory("")]
     public class JsonRpcClient : HttpWebClientProtocol
     {
-        private int _id;
-        private string _projectPath;
+        int id;
+        readonly string projectPath;
 
         public JsonRpcClient(string projectPath)
-            : base()
         {
-            _projectPath = projectPath;
+            this.projectPath = projectPath;
 
-            string coltFolder = System.Environment.GetEnvironmentVariable("USERPROFILE") + @"\.colt\";
+            string coltFolder = string.Format("{0}\\.colt_as\\", Environment.GetEnvironmentVariable("USERPROFILE"));
 
             XmlDocument storageDescriptor = new XmlDocument();
-            storageDescriptor.Load(coltFolder + "storage.xml");
+            storageDescriptor.Load(string.Format("{0}storage.xml", coltFolder));
 
             // <xml>
             //  <storage path='/Users/makc/Desktop/Site/site.colt' subDir='8572a4d3' />
-            XmlNodeList storageList = storageDescriptor.SelectNodes("/xml/storage[@path='" + _projectPath + "']");
+            XmlNodeList storageList = storageDescriptor.SelectNodes(string.Format("/xml/storage[@path='{0}']", this.projectPath));
             if (storageList.Count == 1)
             {
                 string storage = storageList[0].Attributes["subDir"].Value;
-                int port = int.Parse ( File.ReadAllText(coltFolder + @"storage\" + storage + @"\rpc.info").Split(':')[1] );
+                int port = int.Parse ( File.ReadAllText(string.Format("{0}storage\\{1}\\rpc.info", coltFolder, storage)).Split(':')[1] );
 
-                Url = "http://127.0.0.1:" + port + "/rpc/coltService"; return;
+                Url = string.Format("http://127.0.0.1:{0}/rpc/coltService", port); return;
             }
 
             Url = "http://127.0.0.1:8092/rpc/coltService";
@@ -58,20 +61,19 @@
         {
             try
             {
-                Invoke("ping", new Object[] { });
+                Invoke("ping");
             }
-
             catch (Exception)
             {
                 // put it on recent files list
-                string coltFolder = System.Environment.GetEnvironmentVariable("USERPROFILE") + @"\.colt\";
+                string coltFolder = string.Format("{0}\\.colt_as\\", Environment.GetEnvironmentVariable("USERPROFILE"));
 
                 XmlDocument workingSet = new XmlDocument();
-                workingSet.Load(coltFolder + "workingset.xml");
+                workingSet.Load(string.Format("{0}workingset.xml", coltFolder));
                 XmlElement root = (XmlElement)workingSet.SelectSingleNode("/workingset");
                 XmlElement project = (XmlElement)root.PrependChild(workingSet.CreateElement("", "project", ""));
-                project.Attributes.Append(workingSet.CreateAttribute("path")).Value = _projectPath;
-                workingSet.Save(coltFolder + "workingset.xml");
+                project.Attributes.Append(workingSet.CreateAttribute("path")).Value = projectPath;
+                workingSet.Save(string.Format("{0}workingset.xml", coltFolder));
 
                 // open COLT exe
                 Process.Start(executable);
@@ -85,7 +87,7 @@
 
         public virtual object Invoke(string method, params object[] args)
         {
-            Console.WriteLine("Invoke: method = " + method);
+            Console.WriteLine("Invoke: method = {0}", method);
 
             WebRequest request = GetWebRequest(new Uri(Url));
             request.Method = "POST";
@@ -94,7 +96,7 @@
             using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
             {
                 JsonObject call = new JsonObject();
-                call["id"] = ++_id;
+                call["id"] = ++id;
                 call["method"] = method;
                 call["params"] = args;
                 call.Export(new JsonTextWriter(writer));
@@ -104,15 +106,10 @@
             using (Stream stream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
             {
-// String s = reader.ReadToEnd();
-// return s;
                 JsonObject answer = new JsonObject();
                 answer.Import(new JsonTextReader(reader));
-
                 object errorObject = answer["error"];
-
                 if (errorObject != null) OnError(errorObject);
-
                 return answer["result"];
             }
         }
@@ -120,23 +117,13 @@
         protected virtual void OnError(object errorObject)
         {
             JsonObject error = errorObject as JsonObject;
-
-            if (error != null)
-            {
-                string message = error["message"] as string;
-                if (message == null) message = "";
-
-                string exceptionType = null;
-                JsonObject data = error["data"] as JsonObject;
-                if (data != null)
-                {
-                    exceptionType = data["exceptionTypeName"] as string;
-                }
-
-                throw new JsonRpcException(exceptionType, message);
-            }
-
-            throw new Exception(errorObject as string);
+            if (error == null) throw new Exception(errorObject as string);
+            string message = error["message"] as string;
+            if (message == null) message = "";
+            string exceptionType = null;
+            JsonObject data = error["data"] as JsonObject;
+            if (data != null) exceptionType = data["exceptionTypeName"] as string;
+            throw new JsonRpcException(exceptionType, message);
         }
     }
 
@@ -158,19 +145,21 @@
             this.args = args;
             this.callback = callback;
 
-            timer = new Timer();
-            timer.SynchronizingObject = (System.Windows.Forms.Form)PluginCore.PluginBase.MainForm;
-            timer.Interval = 1000;
+            timer = new Timer
+            {
+                SynchronizingObject = (Form) PluginBase.MainForm,
+                Interval = 1000
+            };
             timer.Elapsed += OnTimer;
             count = 0;
             OnTimer();
         }
 
-        private void OnTimer(Object sender = null, EventArgs e = null)
+        void OnTimer(object sender = null, EventArgs e = null)
         {
             timer.Stop();
 
-            Console.WriteLine("JsonRpcClientAsyncHelper's OnTimer(), method = " + method);
+            Console.WriteLine("JsonRpcClientAsyncHelper's OnTimer(), method = {0}", method);
 
             if (COLTIsRunning())
             {
@@ -191,25 +180,23 @@
 
             if (count++ > 13)
             {
-                PluginCore.Managers.TraceManager.Add(Resources.LocaleHelper.GetString("Error.StartingCOLTTimedOut"), -1);
+                TraceManager.Add(LocaleHelper.GetString("Error.StartingCOLTTimedOut"), -1);
                 return;
             }
 
             timer.Start();
         }
 
-        private Boolean COLTIsRunning()
+        bool COLTIsRunning()
         {
             try
             {
-                client.Invoke("ping", new Object[] { });
+                client.Invoke("ping");
                 return true;
             }
-
             catch (Exception)
             {
             }
-
             return false;
         }
     }
--- ColtPlugin/Settings.cs
+++ ColtPlugin/Settings.cs
@@ -1,76 +1,46 @@
 using System;
 using System.ComponentModel;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Text;
 
 namespace ColtPlugin
 {
     [Serializable]
     public class Settings
     {
-        public String SecurityToken;
-
-        private String coltExe = null;
-        private String workingFolder = "colt";
-        private Boolean autorun = true;
-        private Boolean fullConfig = false;
-        private Boolean interceptBuilds = false;
+        public string SecurityToken;
 
         /// <summary> 
         /// Get and sets colt.exe path
         /// </summary>
         [DisplayName("Path to COLT")]
         [Description("Path to COLT executable.")]
-        public String Executable
-        {
-            get { return this.coltExe; }
-            set { this.coltExe = value; }
-        }
+        public string Executable { get; set; }
         
         /// <summary> 
         /// Get and sets colt folder
         /// </summary>
         [DisplayName("COLT Working Folder")]
         [Description("Path to COLT working folder."), DefaultValue("colt")]
-        public String WorkingFolder 
-        {
-            get { return this.workingFolder; }
-            set { this.workingFolder = value; }
-        }
+        public string WorkingFolder { get; set; }
 
         /// <summary> 
         /// Get and sets full autorun flag
         /// </summary>
         [DisplayName("Automatically run COLT project")]
         [Description("Automatically compile and run COLT project after opening it in COLT."), DefaultValue(true)]
-        public Boolean AutoRun
-        {
-            get { return this.autorun; }
-            set { this.autorun = value; }
-        }
+        public bool AutoRun { get; set; }
 
         /// <summary> 
         /// Get and sets full config flag
         /// </summary>
         [DisplayName("Load Full FD Configuration")]
         [Description("Attempt to load full FD configuration in COLT. FD project must be built at least once first."), DefaultValue(false)]
-        public Boolean FullConfig 
-        {
-            get { return this.fullConfig; }
-            set { this.fullConfig = value; }
-        }
+        public bool FullConfig { get; set; }
 
         /// <summary> 
         /// Get and sets production builds flag
         /// </summary>
         [DisplayName("Use COLT for FD builds")]
         [Description("Use COLT fast compiler to build your FD projects."), DefaultValue(false)]
-        public Boolean InterceptBuilds
-        {
-            get { return this.interceptBuilds; }
-            set { this.interceptBuilds = value; }
-        }
+        public bool InterceptBuilds { get; set; }
     }
-
 }
